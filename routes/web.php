742af1518707a4d86deabad6cfea0fdbd2d95fee
Choerul <achmadchoerul12345@gmail.com>
2023-10-30 20:03:19 +0700

    Route_Routes_Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,50 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return 9*9;
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['name' => 'Achmad Choerul Ramdhani', 'phone' => '081294029308']);
+});
+
+// Route::view('/contact', 'contact', ['name' => 'Achmad Choerul Ramdhani', 'phone' => '081294029308']);
+
+Route::redirect('/contact', '/contact-us');
+
+Route::get('/product', function () {
+    return 'product';
+});
+
+Route::get('product/{id}', function ($id) {
+    return view('product.detail', ['id' => $id]);
+});
+
+// name prefixes rute
+Route::prefix('administrator')->group(function (){
+    Route::get('/profil-admin', function() {
+        return 'profil admin';
+    });
+
+    Route::get('/about-admin', function() {
+        return 'about admin';
+    });
+
+    Route::get('/contact-admin', function() {
+        return 'contact admin';
+    });
+
+    Route::get('/profil-admin2', function() {
+        return 'profil admin';
+    });
+
+    Route::get('/about-admin2', function() {
+        return 'about admin';
+    });
+
+    Route::get('/contact-admin2', function() {
+        return 'contact admin';
+    });
+});
