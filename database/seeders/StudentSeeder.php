11013b2c2ee4d5e37f9c92b918c284b6531a268f
Choerul <achmadchoerul12345@gmail.com>
2024-06-15 22:48:03 +0700

    Factory (FAKER)

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -17,26 +17,28 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        Schema::disableForeignKeyConstraints();
-        Student::truncate();
-        Schema::enableForeignKeyConstraints();
+        // Schema::disableForeignKeyConstraints();
+        // Student::truncate();
+        // Schema::enableForeignKeyConstraints();
 
-        $data = [
-            ['name' => 'Achmad', 'gender' => 'L', 'nis' => '220302049', 'class_id' => 3],
-            ['name' => 'Alivia', 'gender' => 'P', 'nis' => '220302050', 'class_id' => 3],
-            ['name' => 'Amalia', 'gender' => 'P', 'nis' => '220302051', 'class_id' => 3],
-            ['name' => 'Apridho', 'gender' => 'L', 'nis' => '220302052', 'class_id' => 3]
-        ];
+        // $data = [
+        //     ['name' => 'Achmad', 'gender' => 'L', 'nis' => '220302049', 'class_id' => 3],
+        //     ['name' => 'Alivia', 'gender' => 'P', 'nis' => '220302050', 'class_id' => 3],
+        //     ['name' => 'Amalia', 'gender' => 'P', 'nis' => '220302051', 'class_id' => 3],
+        //     ['name' => 'Apridho', 'gender' => 'L', 'nis' => '220302052', 'class_id' => 3]
+        // ];
 
-        foreach ($data as $value){
-            Student::insert([
-                'name' => $value['name'],
-                'gender' => $value['gender'],
-                'nis' => $value['nis'],
-                'class_id' => $value['class_id'],
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ]);
-        }
+        // foreach ($data as $value){
+        //     Student::insert([
+        //         'name' => $value['name'],
+        //         'gender' => $value['gender'],
+        //         'nis' => $value['nis'],
+        //         'class_id' => $value['class_id'],
+        //         'created_at' => Carbon::now(),
+        //         'updated_at' => Carbon::now()
+        //     ]);
+        // }
+
+        Student::factory()->count(20)->create();
     }
 }
