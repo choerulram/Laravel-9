0835bd8d46bda92ef6bccd2c4238b5b41428ef4a
Choerul <achmadchoerul12345@gmail.com>
2024-06-16 01:12:49 +0700

    Collection Method

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -13,6 +13,7 @@ class StudentController extends Controller
         // $student = Student::all();
         // return view('student', ['studentsList' => $student]);
 
+
         // QUERY BUILDER
         // $student = DB::table('students')->get(); // tampilkan data
         // dd($student);
@@ -43,5 +44,49 @@ class StudentController extends Controller
         //     'class_id' => 2
         // ]);
         // Student::find(28)->delete();
+
+
+        // COLLECTION
+        // $nilai = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        // $nilaiRataRata = collect($nilai)->avg(); // rata-rata
+        // dd($nilaiRataRata);
+
+        // contains = apakah sebuah array memiliki sesuatu
+        // $nilai = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        // $aaa = collect($nilai)->contains(function ($value) {
+        //     return $value < 6;
+        // });
+        // dd($aaa);
+
+        // diff = mendapatkan perbandingan data dari array satu dan lainnya
+        // $restauranA = ["burger", "siomay", "pizza", "spaghetti", "makaroni", "martabak", "bakso"];
+        // $restauranB = ["pizza", "fried chicken", "martabak", "sayur asem", "pecel lele", "bakso"];
+        // $menuRestoA = collect($restauranA)->diff($restauranB);
+        // $menuRestoB = collect($restauranB)->diff($restauranA);
+        // dd($menuRestoA);
+
+        // filter
+        // $nilai = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        // $aaa = collect($nilai)->filter(function ($value) {
+        //     return $value > 7;
+        // })->all();
+        // dd($aaa);
+
+        // pluck = mengambil semua nilai untuk kunci tertentu
+        // $collection = collect([
+        //     ['nama' => 'budi', 'umur' => 17],
+        //     ['nama' => 'ani', 'umur' => 16],
+        //     ['nama' => 'siti', 'umur' => 17],
+        //     ['nama' => 'rudi', 'umur' => 20],
+        // ]);
+        // $aaa = $collection->pluck('nama')->all();
+        // dd($aaa);
+
+        // map = mengulangi koleksi dan meneruskan setiap nilai ke panggilan balik yang diberikan
+        $nilai = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        $aaa = collect($nilai)->map(function ($value) {
+            return $value * 3;
+        })->all();
+        dd($aaa);
     }
 }
